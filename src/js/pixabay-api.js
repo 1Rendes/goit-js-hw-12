5d126728f92f1656fa51f99b49f196230430fef0
1Rendes <1admin@ukr.net>
2024-05-25 19:57:42 +0200

    rev 1.1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,19 +1,15 @@
 import axios from 'axios';
 export async function fetchImages(input, page) {
-  try {
-    const responce = await axios.get(`https://pixabay.com/api/`, {
-      params: {
-        key: '43920119-17be8655c7213217026175d69',
-        q: input,
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: 'true',
-        per_page: '15',
-        page: page,
-      },
-    });
-    return responce.data;
-  } catch (error) {
-    throw new Error(responce.status);
-  }
+  const responce = await axios.get(`https://pixabay.com/api/`, {
+    params: {
+      key: '43920119-17be8655c7213217026175d69',
+      q: input,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      per_page: '15',
+      page: page,
+    },
+  });
+  return responce.data;
 }
--- src/main.js
+++ src/main.js
@@ -16,7 +16,11 @@ const span = document.querySelector('.loader');
 const showMore = document.querySelector('.show-more');
 let page = 1;
 let totalPage = 0;
-formInput.addEventListener('submit', event => {
+
+formInput.addEventListener('submit', searchHandler);
+showMore.addEventListener('click', showMoreHandler);
+
+async function searchHandler(event) {
   event.preventDefault();
   page = 1;
   imagesDiv.innerHTML = '';
@@ -29,68 +33,63 @@ formInput.addEventListener('submit', event => {
   }
   showMore.classList.add('visually-hidden');
   span.classList.remove('visually-hidden');
-  fetchImages(searchRequest)
-    .then(imagesData => {
-      if (!imagesData.total) {
-        iziToast.error({
-          position: 'topRight',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-        });
-        span.classList.add('visually-hidden');
-        return;
-      }
-      return imagesData;
-    })
-    .then(imagesData => {
-      totalPage = Math.ceil(imagesData.totalHits / 15);
-      imagesDiv.append(...renderImages(imagesData));
-      gallery.refresh();
+  try {
+    const imagesData = await fetchImages(searchRequest, page);
+    if (!imagesData.total) {
+      iziToast.error({
+        position: 'topRight',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
       span.classList.add('visually-hidden');
-      showMore.classList.remove('visually-hidden');
-      if (page === totalPage) {
-        showMore.classList.add('visually-hidden');
-        span.classList.add('visually-hidden');
-        iziToast.success({
-          position: 'topRight',
-          message: "Than's all results for this request.",
-        });
-        return;
-      }
-    })
-    .catch(error => console.log(`Error: ${error}`));
-});
-showMore.addEventListener('click', () => {
-  showMoreHandler();
-});
-function showMoreHandler() {
+      return;
+    }
+    totalPage = Math.ceil(imagesData.totalHits / 15);
+    imagesDiv.innerHTML += renderImages(imagesData);
+    gallery.refresh();
+    span.classList.add('visually-hidden');
+    showMore.classList.remove('visually-hidden');
+    if (page === totalPage) {
+      showMore.classList.add('visually-hidden');
+      span.classList.add('visually-hidden');
+      iziToast.success({
+        position: 'topRight',
+        message: "That's all results for this request.",
+      });
+      return;
+    }
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+}
+
+async function showMoreHandler() {
   showMore.classList.add('visually-hidden');
   span.classList.remove('visually-hidden');
   page++;
   const searchRequest = formInput.elements.input.value.trim();
-  fetchImages(searchRequest, page)
-    .then(imagesData => {
-      imagesDiv.append(...renderImages(imagesData));
-      gallery.refresh();
-      if (page === totalPage) {
-        showMore.classList.add('visually-hidden');
-        span.classList.add('visually-hidden');
-        iziToast.error({
-          position: 'topRight',
-          message: "We're sorry, but you've reached the end of search results.",
-        });
-        return;
-      }
+  try {
+    const imagesData = await fetchImages(searchRequest, page);
+    imagesDiv.innerHTML += renderImages(imagesData);
+    gallery.refresh();
+    if (page === totalPage) {
+      showMore.classList.add('visually-hidden');
       span.classList.add('visually-hidden');
-      const scrollSize =
-        (imagesDiv.firstChild.getBoundingClientRect().height + 24) * 2; //тут я додаю додатково 24 пікселі бо елемент не включає в себе розмір гепу. Але можна переробити верстку на маржін.
-      window.scrollBy({
-        top: scrollSize,
-        behavior: 'smooth',
+      iziToast.error({
+        position: 'topRight',
+        message: "We're sorry, but you've reached the end of search results.",
       });
-      showMore.classList.remove('visually-hidden');
-    })
-    .catch(error => {
-      console.log(`Error: ${error}`);
+      return;
+    }
+    span.classList.add('visually-hidden');
+    const scrollSize =
+      (imagesDiv.firstChild.getBoundingClientRect().height + 24) * 2; //тут я додаю додатково 24 пікселі бо елемент не включає в себе розмір гепу. Але можна переробити верстку на маржін.
+    window.scrollBy({
+      top: scrollSize,
+      behavior: 'smooth',
     });
+    showMore.classList.remove('visually-hidden');
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
 }
